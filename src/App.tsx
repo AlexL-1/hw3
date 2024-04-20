1e95e3651483cacac961be77a13ce00eb9174a8a
Alex L <alex@tor.ru>
2024-04-20 12:48:00 +0300

    to dist

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/index.scss"; // импорт стилей
 import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
